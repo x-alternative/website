c82badf43959e2af28fe28d5a40481cdf4e34338
Portalez Régis <11440561+JeWaVe@users.noreply.github.com>
2023-04-03 15:36:33 +0200

    Update author.php
    add images
    add pagination
    longer excerpt

--- twentyfifteen-xalt/author.php
+++ twentyfifteen-xalt/author.php
@@ -17,19 +17,25 @@
 				<div class="xalt-author-posts">
 				
 				<!-- The Loop -->
-
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 						<li>
 							<span class="xalt-post-date"><?php the_time('d F Y') ?> : </span>
+							<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
 							<a class="xalt-post-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+							
+							<img src="<?php echo $url ?>" />
 							<div class="xalt-post-abstract"><?php
 								$str = apply_filters( 'the_content', get_the_content() );
 								$start = strpos($str, '<p>');
 								$stop = strpos($str, '</p>');
+								if ($stop - $start < 128)
+									$stop = strpos($str, '</p>', $stop+1);
+								if ($stop - $start < 128)
+									$stop = strpos($str, '</p>', $stop+1);
 								$firstPar = substr($str, $start, $stop - $start + 4);
 								echo $firstPar;
 							?><p>...</p></div>
-                                                        <a class="xalt-read-more" href="<?php the_permalink() ?>">Lire en entier</a>
+                            <a class="xalt-read-more" href="<?php the_permalink() ?>">Lire en entier</a>
 						</li>
 
 					<?php endwhile; else: ?>
@@ -38,6 +44,7 @@
 					<?php endif; ?>
 
 				<!-- End Loop -->
+				<?php the_posts_pagination() ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
